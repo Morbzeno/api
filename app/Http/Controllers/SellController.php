562fba6924c34b59e2a53ce290a00146260ea7b8
Luis <2123100609@soy.utj.edu.mx>
2025-03-14 10:37:15 -0600

    validaciones al cart

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -22,8 +22,8 @@ public function store(Request $request, $id)
     try {
         // Validar los datos
         $validated = $request->validate([
-           //  'direction_id' => 'required|exists:directions,id',
-           //  'purchase_method' => 'nullable|string',
+             'direction_id' => 'required|exists:directions,id',
+             'purchase_method' => 'nullable|string',
         ]);
 
         // Obtener el carrito activo del cliente
@@ -64,9 +64,9 @@ public function store(Request $request, $id)
         $sell = Sell::create([
             'cart_id' => $cart->id,
             'client_id' => $id,
-           // 'direction_id' => $request->direction_id,
+            'direction_id' => $request->direction_id,
             'iva' => $iva,
-           // 'purchase_method' => $request->purchase_method,
+            'purchase_method' => $request->purchase_method,
         ]);
 
         // Reducir el stock de cada producto ahora que sabemos que hay suficiente
